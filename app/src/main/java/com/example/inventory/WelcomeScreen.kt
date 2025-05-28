d408f6f898d78c5476b62b6d71224780efc40bf3
Yashraj Omar <2023eeb1258@iitrpr.ac.in>
2025-05-28 15:27:19 +0530

    medical_first

---
+++ app/src/main/java/com/example/inventory/WelcomeScreen.kt
@@ -0,0 +1,83 @@
+package com.example.inventory
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import com.example.inventory.databinding.ActivityWelcomeScreenBinding
+
+/*
+class WelcomeScreen : AppCompatActivity() {
+    private val binding : ActivityWelcomeScreenBinding by lazy{
+        ActivityWelcomeScreenBinding.inflate(layoutInflater)
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    Handler(Looper.getMainLooper()).postDelayed({
+        startActivity(Intent(this, LoginActivity:: class.java))
+        finish()
+    }, 3000)
+
+        val welcomeText = "Welcome"
+        val spannableString = SpannableString(welcomeText)
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#0060F7")),0,5,0)
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#FFFFFF")),5,welcomeText.length,0)
+
+        binding.welcomeText.text = spannableString
+        //binding.welcomeText.text = "WELCOME"
+    }
+}
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage(modifier: Modifier, navController: NavController
+             , authViewModel: AuthViewModel){
+    val authState = authViewModel.authState.observeAsState()
+
+    LaunchedEffect(authState.value) {
+        when(authState.value){
+            is AuthState.Unauthenticated -> navController.navigate("login")
+            else -> Unit
+        }
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Page", fontSize = 32.sp)
+
+        TextButton(onClick = {
+            authViewModel.signout()
+        }) {
+            Text(text = "Sign out")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/inventory/ui/navigation/InventoryNavGraph.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.inventory.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.inventory.AuthViewModel
+import com.example.inventory.LoginDestination
+import com.example.inventory.LoginPage
+import com.example.inventory.SignupDestination
+import com.example.inventory.SignupPage
+import com.example.inventory.ui.home.HomeDestination
+import com.example.inventory.ui.home.HomeScreen
+import com.example.inventory.ui.item.ItemDetailsDestination
+import com.example.inventory.ui.item.ItemDetailsScreen
+import com.example.inventory.ui.item.ItemEditDestination
+import com.example.inventory.ui.item.ItemEditScreen
+import com.example.inventory.ui.item.ItemEntryDestination
+import com.example.inventory.ui.item.ItemEntryScreen
+
+/**
+ * Provides Navigation graph for the application.
+ */
+@Composable
+fun InventoryNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = LoginDestination.route,
+        modifier = modifier
+    ) {
+
+        composable(LoginDestination.route) {
+            LoginPage(modifier,navController,
+                authViewModel = AuthViewModel(),
+                navigateToSignup = { navController.navigate(SignupDestination.route) })
+        }
+        composable(SignupDestination.route) {
+            SignupPage(modifier,navController, authViewModel = AuthViewModel())
+        }
+        /*composable("home") {
+            LoginPage(modifier,navController, authViewModel = AuthViewModel())
+        }
+         */
+
+        composable(route = HomeDestination.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(ItemEntryDestination.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${ItemDetailsDestination.route}/${it}")
+                }
+            )
+        }
+        composable(route = ItemEntryDestination.route) {
+            ItemEntryScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+        composable(
+            route = ItemDetailsDestination.routeWithArgs,
+            arguments = listOf(navArgument(ItemDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            ItemDetailsScreen(
+                navigateToEditItem = { navController.navigate("${ItemEditDestination.route}/$it") },
+                navigateBack = { navController.navigateUp() }
+            )
+        }
+        composable(
+            route = ItemEditDestination.routeWithArgs,
+            arguments = listOf(navArgument(ItemEditDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            ItemEditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+    }
+}
